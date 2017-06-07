cbc17995bc35c28cd529f368083518ac2121ff3c
Inti Velasquez <intiv@github.com>
2017-06-07 16:56:24 -0600

    controller iniciado

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,20 +1,43 @@
-var student = require('../schemas/student');
+var book = require('../schemas/book');
+var mongoose = require('mongoose');
 
-exports.getStudents = {
+exports.getBooks = {
   handler: function(request, reply){
     var books = book.find({});
-    reply(students);
+    reply(books);
   }
 }
 
-exports.createStudent = {
+exports.createBook = {
   handler: function(request, reply){
-    var newBook = new student({
-      name: request.payload.name,
-      account: request.payload.account
+    var newBook = new book({
+      titulo : request.payload.titulo,
+      genero : request.payload.genero,
+      autor : request.payload.autor,
+      publicacion : request.payload.publicacion,
+      editorial : request.payload.editorial,
+      descripcion : request.payload.descripcion,
+      keywords : request.payload.keywords,
+      copias_total : request.payload.copias_total,
+      copias_disponible : request.payload.copias_disponible
     });
-    newStudent.save();
+    newBook.save();
     console.log('student saved');
     return reply('ok');
+  }
+}
+
+exports.deleteBook = {
+  handler: function(request, reply){
+    book.remove({_id : request.params._id}, function(err, result){
+      if(err){
+        return reply(boom.wrap(err, 'Delete failed'));
+      }
+      if(result.n === 0){
+        return reply(boom.notFound());
+      }else{
+        return reply().code(204);
+      }
+    })
   }
 }
